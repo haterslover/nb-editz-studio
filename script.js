5b7426b654614fd229346a6da1f46e3207bf2557
Nagesh Bikkad <bikkadnagesh@gmail.com>
2026-06-26 18:48:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+const words=[
+"Video Editor",
+"Colorist",
+"BGMI Esports Editor",
+"Content Creator",
+"DaVinci Resolve Artist"
+];
+
+let word=0;
+let letter=0;
+let deleting=false;
+
+const typing=document.getElementById("typing");
+
+function type(){
+
+let current=words[word];
+
+if(!deleting){
+
+typing.innerHTML=current.substring(0,letter++);
+
+if(letter>current.length){
+
+deleting=true;
+
+setTimeout(type,1200);
+
+return;
+
+}
+
+}else{
+
+typing.innerHTML=current.substring(0,letter--);
+
+if(letter<0){
+
+deleting=false;
+
+word=(word+1)%words.length;
+
+}
+
+}
+
+setTimeout(type,deleting?60:120);
+
+}
+
+type();
